b33a47ed10066b646ec93d919b5213e7e537071a
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-07-10 15:59:03 +0100

    Refactor CSVReader.cs file

--- AddressProcessor/CSV/CSVReader.cs
+++ AddressProcessor/CSV/CSVReader.cs
@@ -11,46 +11,14 @@ namespace AddressProcessing
 
         public StreamReader _readerStream = null;
 
-        public bool Read(string column1, string column2)
-        {
-            const int FIRST_COLUMN = 0;
-            const int SECOND_COLUMN = 1;
-
-            string line;
-            string[] columns;
-
-            char[] separator = { '\t' };
-
-            line = ReadLine();
-            columns = line.Split(separator);
-
-            if (columns.Length == 0)
-            {
-                column1 = null;
-                column2 = null;
-
-                return false;
-            }
-            else
-            {
-                column1 = columns[FIRST_COLUMN];
-                column2 = columns[SECOND_COLUMN];
-
-                return true;
-            }
-        }
-
         public bool Read(out string column1, out string column2)
         {
             const int FIRST_COLUMN = 0;
             const int SECOND_COLUMN = 1;
 
-            string line;
-            string[] columns;
-
             char[] separator = { '\t' };
-
-            line = ReadLine();
+            string line = ReadLine();
+            string[] columns = line.Split(separator);
 
             if (line == null)
             {
@@ -59,8 +27,6 @@ namespace AddressProcessing
 
                 return false;
             }
-
-            columns = line.Split(separator);
 
             if (columns.Length == 0)
             {
