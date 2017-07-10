ab14d2784aaa6d7b84397dbbcb8ee805397b6f5a
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-07-10 15:52:45 +0100

    Extract functions related to Reader, moved them to the CSVReader.cs file

--- AddressProcessor/CSV/CSVReader.cs
+++ AddressProcessor/CSV/CSVReader.cs
@@ -8,5 +8,79 @@ namespace AddressProcessing
 {
     class CSVReader
     {
+
+        public StreamReader _readerStream = null;
+
+        public bool Read(string column1, string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        public bool Read(out string column1, out string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+
+            if (line == null)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            } 
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        public string ReadLine()
+        {
+            return _readerStream.ReadLine();
+        }
     }
 }
